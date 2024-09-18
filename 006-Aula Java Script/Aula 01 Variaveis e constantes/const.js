78d2e2afe0287a4e0db5805472a432b4dfcd4ad7
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-09-18 01:03:15 -0300

    Primeiro commit na branch aulaJavaScript

---
+++ 006-Aula Java Script/Aula 01 Variaveis e constantes/const.js
@@ -0,0 +1,6 @@
+// coloquei '//' antes dos codigos com erro de sintaxe pq se nao quando for ver no console vai aparecer apenas a mensagem de erro
+
+//const animal; // criando uma variavel const da maneira errada
+const animal = 'gato' //criando uma variavel const da maneira certa
+
+console.log(animal);
---
+++ 006-Aula Java Script/Aula 01 Variaveis e constantes/let.js
@@ -0,0 +1,13 @@
+let fruta; // criando variavel fruta sem valor nenhum
+
+console.log(fruta); //mostrando o valor de fruta no console
+
+let fruta2 = 'maça'; // criando variavel fruta2 com valor 'maça'
+
+console.log(fruta2); //mostrando o valor de fruta2 no console
+
+fruta = 'banana' // adicionando valor a fruta
+
+console.log(fruta, fruta2); //mostrando o valor de fruta e fruta2 no console
+
+
