72d9cfca484c5b236f4ba270499d9f26c9ee4a21
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-09-18 23:30:28 -0300

    adicionando variaveis dos 4 tipos mais comuns que sao: int,float, string e boolean

--- 006-Aula Java Script/Aula 02 Tipos de dados/script.js
+++ 006-Aula Java Script/Aula 02 Tipos de dados/script.js
@@ -1,1 +1,18 @@
-refw
+let number1 = 15;
+console.log(typeof(number1))
+
+let number2 = 5.87;
+console.log(typeof(number2))
+
+let number3 = '6'
+console.log(typeof(number3))
+
+let fruta = 'maça';
+console.log(typeof(fruta))
+
+let banho = true;
+console.log(typeof(banho))
+
+let fome = false;
+console.log(typeof(fome))
+
