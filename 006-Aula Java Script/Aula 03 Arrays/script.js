d03a0112b2e17d8ea7f4755fa595d3e8191bfb1d
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-09-19 19:50:35 -0300

    adicionando comentarios ao meu codigo

--- 006-Aula Java Script/Aula 03 Arrays/script.js
+++ 006-Aula Java Script/Aula 03 Arrays/script.js
@@ -1,17 +1,17 @@
-let heroes = []
-console.log(heroes)
+let heroes = [] // criando array vazio
+console.log(heroes) // mostrando array que acabei de criar
 
 
-let vilians = ['Coringa', 'Pinguin', '2 caras']
-console.log(vilians);
+let vilians = ['Coringa', 'Pinguin', '2 caras'] // criando array com 3 itens
+console.log(vilians); // mostrando o ultimo array que eu criei
 
-console.log(vilians.length)
+console.log(vilians.length) // mostrando a quantidade de itens que tem no array
 
-heroes.push('Batman')
-heroes.push('Robin', 'Bat-Girl')
-console.log(heroes)
-console.log(heroes.length)
+heroes.push('Batman') // adicionando item no meu array de herois
+heroes.push('Robin', 'Bat-Girl') // adicionando mais 2 itens ao meu array
+console.log(heroes) //mostrando o array
+console.log(heroes.length) // mostrando a quantidade de itens no array
 
 
-heroes.splice(2,1)
-console.log(heroes);
+heroes.splice(2,1) // removendo 1 item apartir do index 2 
+console.log(heroes); // mostrando o array com o item removido
