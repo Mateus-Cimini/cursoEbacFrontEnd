8aba6ffea56e9f63b88fe9afc39b922adf9f7103
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-09-19 22:47:50 -0300

    adicionando comentarios ao codigo

--- 006-Aula Java Script/aula 04 Laços/script.js
+++ 006-Aula Java Script/aula 04 Laços/script.js
@@ -1,12 +1,12 @@
-let champions = ['Ezreal', 'Kaisar', 'Jinx', 'Zeri']
+let champions = ['Ezreal', 'Kaisar', 'Jinx', 'Zeri']; // Declara um array com nomes de campeões
 
-for(let i = 0; i < champions.length; i++) {
-    console.log('O meu campeão é: ' + champions[i]);
+for(let i = 0; i < champions.length; i++) { // Loop 'for' percorre o array de campeões
+    console.log('O meu campeão é: ' + champions[i]); // Exibe o nome de cada campeão, um por um
 }
 
-let phrase = 0
+let phrase = 0; // Inicializa a variável 'phrase' com o valor 0
 
-while(phrase < champions.length) {
-    console.log('O neu campeão preferido é: ' + champions[phrase]);
-    phrase++
+while(phrase < champions.length) { // Loop 'while' continua enquanto 'phrase' for menor que o comprimento do array
+    console.log('O meu campeão preferido é: ' + champions[phrase]); // Exibe o nome do campeão preferido, com base no valor de 'phrase'
+    phrase++; // Incrementa 'phrase' para passar ao próximo campeão
 }
