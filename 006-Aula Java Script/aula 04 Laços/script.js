9535728dbc56dce922724ac03cb267cb79adc7cf
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-09-19 22:39:26 -0300

    adicionando loop de for e while para pecorrer um array de string e mostrar no console uma frase personalizada

---
+++ 006-Aula Java Script/aula 04 Laços/script.js
@@ -0,0 +1,12 @@
+let champions = ['Ezreal', 'Kaisar', 'Jinx', 'Zeri']
+
+for(let i = 0; i < champions.length; i++) {
+    console.log('O meu campeão é: ' + champions[i]);
+}
+
+let phrase = 0
+
+while(phrase < champions.length) {
+    console.log('O neu campeão preferido é: ' + champions[phrase]);
+    phrase++
+}
