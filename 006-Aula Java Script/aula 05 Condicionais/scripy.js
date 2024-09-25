9052db3ef4ba093606275cf78f55376c6cdcc053
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-09-24 23:20:24 -0300

    codigo ja terminado

--- 006-Aula Java Script/aula 05 Condicionais/scripy.js
+++ 006-Aula Java Script/aula 05 Condicionais/scripy.js
@@ -1,11 +1,17 @@
 let idade = prompt('Qual a sua idade?');
 
-
 if (idade >= 16) {
-    console.log('Você esta na idade de votar');
+    alert('Você esta na idade de votar');
 }
 
 else {
-    console.log('Voce não tem idade suficiente para votar');
-    
+    alert('Voce não tem idade suficiente para votar');
 }
+
+// METODO 2
+
+let idade2 = prompt('teste 2 : Qual a sua idade?')
+
+const resultado = idade >= 16 ? 'Você esta na idade de votar' : 'Voce não tem idade suficiente para votar'
+
+alert(resultado)
