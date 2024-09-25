53abae48d6125acb6a2a2325b43a299f77c5df6a
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-09-24 23:13:43 -0300

    criando area de trabalho da aula de condicionais

---
+++ 006-Aula Java Script/aula 05 Condicionais/scripy.js
@@ -0,0 +1,11 @@
+let idade = prompt('Qual a sua idade?');
+
+
+if (idade >= 16) {
+    console.log('Você esta na idade de votar');
+}
+
+else {
+    console.log('Voce não tem idade suficiente para votar');
+    
+}
