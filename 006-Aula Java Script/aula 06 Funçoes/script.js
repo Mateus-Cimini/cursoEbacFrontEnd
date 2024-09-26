1cf9a3165353ca57ed9d2532ca5eecc032a63ba3
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-09-26 17:47:30 -0300

    deixando mais agradavel para o usuario

--- 006-Aula Java Script/aula 06 Funçoes/script.js
+++ 006-Aula Java Script/aula 06 Funçoes/script.js
@@ -7,11 +7,25 @@ function descobrirIdade(anoNacimento) {
 console.log(descobrirIdade(2020));
 */
 
+/*
 //FUNÇAO COM 3 PARAMETROS
 function somarNumeros(n1, n2, n3) {
     return n1 + n2 + n3
 }
 
 console.log(somarNumeros(10, 20, 20));
+*/
+
+
+
+//FAZENDO A FUNÇAO ACIMA FICCAR MAIS AGRADAVEL PARA O USUARIO
+
+function somarNumeros2(n1, n2, n3) {
+    let resultado = n1 + n2 + n3
+    return console.log('O resultado da soma dos 3 valores e de: ' + resultado);
+};
+
+somarNumeros2(1, 5, 4)
+
 
 
