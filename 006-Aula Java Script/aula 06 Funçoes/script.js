c419d94efe2354a768b019d980d6291c756f67b0
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-09-26 18:00:20 -0300

    deixando mais agradavel para o usuario usando prompt e alert

--- 006-Aula Java Script/aula 06 Funçoes/script.js
+++ 006-Aula Java Script/aula 06 Funçoes/script.js
@@ -16,16 +16,24 @@ function somarNumeros(n1, n2, n3) {
 console.log(somarNumeros(10, 20, 20));
 */
 
-
-
+/*
 //FAZENDO A FUNÇAO ACIMA FICCAR MAIS AGRADAVEL PARA O USUARIO
-
 function somarNumeros2(n1, n2, n3) {
     let resultado = n1 + n2 + n3
     return console.log('O resultado da soma dos 3 valores e de: ' + resultado);
 };
 
 somarNumeros2(1, 5, 4)
+*/
 
 
-
+//USANDO FUNÇOES PARA USADO NO NAVEGADOR
+let n1 = parseInt(prompt('Digite o valor 1'))
+let n2 = parseInt(prompt('Digite o valor 2'))
+let n3 = parseInt(prompt('Digite o valor 3' ))
+function somarNumeros3(n1, n2, n3) {
+    let resultado = n1 + n2 + n3
+    return alert (`${n1} + ${n2} + ${n3} = ${resultado}`)
+    
+}
+somarNumeros3(n1,n2,n3)
